1bdaeff5c8793c540c7efa8e274db9604f6bdc94
Honman Yau <honmanyau@users.noreply.github.com>
2017-04-19 12:40:58 +1000

    Added Grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Grid from './Grid';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>The Adventure of Muup</h1>
+        <Grid x="1" y="1" terrain="0" roomId="0" player="0" />
       </div>
     );
   }
